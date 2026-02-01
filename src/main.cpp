f86dcbe5f5cfe522455beebb1e3182dbafa795e4
ADSJohn <progjohn@outlook.com>
2026-02-01 17:04:05 -0300

    adicao dos 3 graficos

--- src/main.cpp
+++ src/main.cpp
@@ -5,115 +5,81 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const char *ssid = "";
-const char *password = "";
-
-const char *mqtt_server = "broker.hivemq.com";
-const int mqtt_port = 1883;
-
-const char *topicSensores = "eletromecanica/motor/sensores";
-const char *topicStatus = "eletromecanica/motor/status";
-
 #define MMA8452_ADDR 0x1C
+#define HMC5883_ADDR 0x1E
 #define ONE_WIRE_BUS D4
+
+const char *ssid = "Espaco_Vitoria";
+const char *password = "espacovitoria";
+const char *mqtt_server = "broker.hivemq.com";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature tempSensor(&oneWire);
 
-void writeRegister(byte addr, byte reg, byte value)
+void writeReg(byte addr, byte reg, byte val)
 {
   Wire.beginTransmission(addr);
   Wire.write(reg);
-  Wire.write(value);
+  Wire.write(val);
   Wire.endTransmission();
-}
-
-void reconnectMQTT()
-{
-  while (!client.connected())
-  {
-    if (client.connect("ESP8266_MOTOR_PREDITIVA"))
-    {
-      client.publish(topicStatus, "ESP8266 conectado - 200Hz ativo");
-    }
-    else
-    {
-      delay(1000);
-    }
-  }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  delay(2000);
-
-  Serial.println("\nInicializando sistema preditivo...");
-
   Wire.begin(D2, D1);
-  Wire.setClock(400000);
 
   tempSensor.begin();
 
-  writeRegister(MMA8452_ADDR, 0x2A, 0x00);
-  writeRegister(MMA8452_ADDR, 0x0E, 0x00);
-  writeRegister(MMA8452_ADDR, 0x2A, 0x01);
+  writeReg(MMA8452_ADDR, 0x2A, 0x00);
+  writeReg(MMA8452_ADDR, 0x0E, 0x00);
+  writeReg(MMA8452_ADDR, 0x2A, 0x01);
+
+  writeReg(HMC5883_ADDR, 0x00, 0x70);
+  writeReg(HMC5883_ADDR, 0x01, 0x20);
+  writeReg(HMC5883_ADDR, 0x02, 0x00);
 
   WiFi.begin(ssid, password);
-  Serial.print("Conectando WiFi");
   while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(300);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi conectado!");
+    delay(500);
 
-  client.setServer(mqtt_server, mqtt_port);
+  client.setServer(mqtt_server, 1883);
 }
 
 void loop()
 {
-  static unsigned long lastSample = 0;
-  const unsigned long interval = 5;
-
   if (!client.connected())
-    reconnectMQTT();
-  client.loop();
+    client.connect("ESP_MOTOR");
 
-  unsigned long now = micros();
+  tempSensor.requestTemperatures();
+  float temp = tempSensor.getTempCByIndex(0);
 
-  if (now - lastSample >= interval * 1000)
-  {
-    lastSample = now;
+  Wire.beginTransmission(MMA8452_ADDR);
+  Wire.write(0x01);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MMA8452_ADDR, 6);
 
-    static int tempCounter = 0;
-    static float temp = 0;
+  int ax = (Wire.read() << 8 | Wire.read()) >> 4;
+  int ay = (Wire.read() << 8 | Wire.read()) >> 4;
+  int az = (Wire.read() << 8 | Wire.read()) >> 4;
 
-    if (++tempCounter >= 200)
-    {
-      tempSensor.requestTemperatures();
-      temp = tempSensor.getTempCByIndex(0);
-      tempCounter = 0;
-    }
+  Wire.beginTransmission(HMC5883_ADDR);
+  Wire.write(0x03);
+  Wire.endTransmission();
+  Wire.requestFrom(HMC5883_ADDR, 6);
 
-    Wire.beginTransmission(MMA8452_ADDR);
-    Wire.write(0x01);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MMA8452_ADDR, 6);
+  int mx = Wire.read() << 8 | Wire.read();
+  Wire.read();
+  Wire.read();
+  int my = Wire.read() << 8 | Wire.read();
 
-    int16_t ax = (Wire.read() << 8 | Wire.read()) >> 4;
-    int16_t ay = (Wire.read() << 8 | Wire.read()) >> 4;
-    int16_t az = (Wire.read() << 8 | Wire.read()) >> 4;
+  char payload[256];
+  snprintf(payload, sizeof(payload),
+           "{\"temp\":%.2f,\"ax\":%d,\"ay\":%d,\"az\":%d,\"mx\":%d,\"my\":%d}",
+           temp, ax, ay, az, mx, my);
 
-    char payload[128];
-    snprintf(payload, sizeof(payload),
-             "{\"temp\":%.2f,\"ax\":%d,\"ay\":%d,\"az\":%d}",
-             temp, ax, ay, az);
-
-    client.publish(topicSensores, payload);
-    Serial.println(payload);
-  }
+  client.publish("eletromecanica/motor/raw", payload);
+  delay(5); // 200Hz
 }
